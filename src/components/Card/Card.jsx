2557f040312e8452c0f1654b2fbe603020fe0104
Samridh <samridht23@gmail.com>
2022-04-18 22:55:59 +0530

    body and card fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,14 +6,14 @@ const Card = (props) => {
     <div>
       <div className="main-card">
         <div className="card">
-          <div className="card-img">
+          <div className="movie-img">
             <img src={props.medium_cover_image} alt="movie-poster" />
           </div>
-          <div className="card-title">
+          <div className="movie-title">
             <a href="#">{props.title}</a>
           </div>{" "}
           {/*use react router Link to route link*/}
-          <div className="card-desc">{props.year}</div>
+          <div className="movie-year">{props.year}</div>
         </div>
       </div>
     </div>
