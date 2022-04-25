6ec6712f31409921d18d0be19be252cfa450a255
Samridh <samridht23@gmail.com>
2022-04-25 08:09:22 +0530

    Movie page updated

--- src/routes/MoviePage/MoviePage.jsx
+++ src/routes/MoviePage/MoviePage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./style.scss";
 import Navbar from "../../components/Navbar/Navbar";
+import { useState, useEffect } from "react";
+
+const baseUrl = "https://api.themoviedb.org/3/";
 
 const MoviePage = () => {
+  const [moviedata, setMovieData] = React.useState([]);
   return (
     <>
       <Navbar />
