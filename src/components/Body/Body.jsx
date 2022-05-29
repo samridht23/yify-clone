1fe9a76c3d099ef55be47da7ac17bd0318af4290
Samridh <samridht23@gmail.com>
2022-05-29 09:36:39 +0530

    loading added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,42 +2,45 @@ import React, { useState, useEffect } from "react";
 import "./style.scss";
 import Card from "../Card/Card";
 import axios from "axios";
+import Loading from "../Loading/Loading";
 
 const baseURL = "https://yts.mx/api/v2/list_movies.json";
 
 const Body = () => {
   const [moviedata, setMoviedata] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = () => {
     axios
       .get(baseURL)
       .then((res) => {
         setMoviedata(res.data.data.movies);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
   useEffect(() => {
     fetchData();
   }, []);
-
-  return (
-    <div>
-      <div className="main-body">
-        <div className="movie-grid">
-          {moviedata.map((moviedata) => (
-            <Card
-              medium_cover_image={moviedata.medium_cover_image}
-              title={moviedata.title}
-              year={moviedata.year}
-              movie_id={moviedata.id}
-            />
-          ))}
-        </div>
+  const MovieFetch = (
+    <div className="main_body">
+      <div className="movie_grid">
+        {moviedata.map((moviedata, index) => (
+          <Card
+            key={index}
+            medium_cover_image={moviedata.medium_cover_image}
+            title={moviedata.title}
+            year={moviedata.year}
+            movie_id={moviedata.id}
+          />
+        ))}
       </div>
     </div>
   );
+  return <div className="main_app">{isLoading ? <Loading /> : MovieFetch}</div>;
 };
 
 export default Body;
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import { useState, React } from "react";
+import "./style.scss";
+
+const Loading = () => {
+  return (
+    <div className="main_loading">
+      <div class="loader"></div>
+    </div>
+  );
+};
+export default Loading;
